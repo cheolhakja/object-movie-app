7e3b1a65a08f412b37ac41e4cff6e09a39568e37
jeon-ji-yoon <rebegin15@gmail.com>
2021-07-09 20:05:10 +0900

    refactor: Theater의 enter 메소드를 TicketSeller 내부로 옮기기

--- src/main/java/hello/object/TicketSeller.java
+++ src/main/java/hello/object/TicketSeller.java
@@ -7,7 +7,15 @@ public class TicketSeller {
         this.ticketOffice = ticketOffice;
     }
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience){
+        if(audience.getBag().hasInvitation()){
+            Ticket ticket = this.ticketOffice.getTickets();
+            audience.getBag().setTicket(ticket);
+        }else{
+            Ticket ticket=this.ticketOffice.getTickets();
+            audience.getBag().minusAmount(ticket.getFee());
+            this.ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
