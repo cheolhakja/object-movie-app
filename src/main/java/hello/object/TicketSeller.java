0ab1618548485d95046268c2ae93a76d3eb6b924
jeon-ji-yoon <rebegin15@gmail.com>
2021-07-10 14:35:35 +0900

    refactor: Audience가 직접 Bag에 접근하도록 함

--- src/main/java/hello/object/TicketSeller.java
+++ src/main/java/hello/object/TicketSeller.java
@@ -8,14 +8,6 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience){
-        if(audience.getBag().hasInvitation()){
-            Ticket ticket = this.ticketOffice.getTickets();
-            audience.getBag().setTicket(ticket);
-        }else{
-            Ticket ticket=this.ticketOffice.getTickets();
-            audience.getBag().minusAmount(ticket.getFee());
-            this.ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        this.ticketOffice.plusAmount(audience.buy(ticketOffice.getTickets()));
     }
 }
